9f791fcfc039992d01c77acd13ee4c6770491a7d
Arthur Silva <arthurmatsilva@outlook.com>
2021-01-11 11:59:43 -0300

    Marks placing and player change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const gameBoard = (function () {
-  const gameboard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+  const gameboard = ['', '', '', '', '', '', '', '', ''];
 
   const getGameBoard = () => gameboard;
   const setGameBoard = (position, marker) => gameboard[position] = marker;
@@ -10,18 +10,54 @@ const gameBoard = (function () {
   };
 })();
 
+function player(name, mark) {
+  const getName = () => name;
+  const setName = (newName) => name = newName;
+  const getMark = () => mark;
+  const setMark = (newMark) => mark = newMark;
+
+  return {
+    getName,
+    setName,
+    getMark,
+    setMark
+  };
+}
+
 const displayController = (function(gameboard) {
+  const grid = document.querySelectorAll('#gameboard button');
+  const turn = document.querySelector('#main-header h2');
+
+  const player1 = player('Player1', 'X');
+  const player2 = player('Player2', 'O');
+  let actualPlayer;
+  changePlayer();
+
+  grid.forEach((position, index) => {
+    position.addEventListener('click', () => {
+      if (!position.textContent) {
+        gameboard.setGameBoard(index, actualPlayer.getMark());
+        changePlayer();
+        renderGameBoard();
+      }       
+    });
+  });
+
+  function changePlayer() {
+    if (!actualPlayer) {
+      actualPlayer = player1;
+    } else if (actualPlayer.getName() === player1.getName()) {
+      actualPlayer = player2;
+    } else {
+      actualPlayer = player1;
+    }
+    turn.textContent = `${actualPlayer.getName()}'s turn`;
+  }
+
   const renderGameBoard = () => {
-    const positions = document.querySelectorAll('#gameboard button');
     const board = gameboard.getGameBoard();
-    positions.forEach((position, index) => position.textContent = board[index]);
+    grid.forEach((position, index) => position.textContent = board[index]);
   }
 
   return { renderGameBoard };
 })(gameBoard);
-
-function player(name, marker) {
-
-}
-
-displayController.renderGameBoard();
