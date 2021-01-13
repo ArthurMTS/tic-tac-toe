2443dd4edbade8b9c60438ed12651274ec7f8d5c
Arthur Silva <arthurmatsilva@outlook.com>
2021-01-13 19:01:43 -0300

    checkWinner function

--- js/main.js
+++ js/main.js
@@ -23,7 +23,12 @@ const displayController = (function(gb) {
     turn.textContent = `${player.getName()}'s turn`;
   }
 
-  return { renderGameBoard, displayPlayer, grid };
+  return { 
+    renderGameBoard,
+    displayPlayer, 
+    grid
+  };
+
 })(gameBoard);
 
 function player(name, mark) {
@@ -56,6 +61,7 @@ const gameController = (function(gb, dc) {
   function mark(index) {
     if (!this.textContent) {
       gb.setGameBoard(index, actualPlayer.getMark());
+      checkWinner(actualPlayer);
       changePlayer();
       dc.displayPlayer(actualPlayer);
       dc.renderGameBoard();
@@ -73,4 +79,21 @@ const gameController = (function(gb, dc) {
     return actualPlayer;
   }
 
+  function checkWinner(player) {
+    const board = gb.getGameBoard();
+
+    if ((board[0] === board[1] && board[1] === board[2] && board[0]) ||
+        (board[3] === board[4] && board[4] === board[5] && board[3]) ||
+        (board[6] === board[7] && board[7] === board[8] && board[6]) ||
+        (board[0] === board[3] && board[3] === board[6] && board[0]) ||
+        (board[1] === board[4] && board[4] === board[7] && board[1]) ||
+        (board[2] === board[5] && board[5] === board[8] && board[2]) ||
+        (board[0] === board[4] && board[4] === board[8] && board[0]) ||
+        (board[2] === board[4] && board[4] === board[6]) && board[2]) {
+          console.log('Winner ' + player.getName());
+        }
+  }
+
 })(gameBoard, displayController);
+
+displayController.renderGameBoard();
